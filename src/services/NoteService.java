bb7b005f01b4e1c7bc77d875cef39d19497e48aa
dev1 <dev1@example.com>
2022-08-29 11:43:56 +0300

    Rewriting noteService according to OOP standards

--- src/services/NoteService.java
+++ src/services/NoteService.java
@@ -3,19 +3,49 @@ package services;
 import models.Note;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class NoteService {
 
+    private List<Note> allNotes = new ArrayList<>();
+
     public NoteService() {
+        loadNotesToList();
     }
+
+    public void loadNotesToList() {
+        try {
+            File fileFolder = new File(System.getProperty("user.dir") + "/notes");
+            File[] files = fileFolder.listFiles();
+            String toDel = System.getProperty("user.dir") + "/notes/";
+
+            for (File file : files) {
+                String fileName = file.toString().replaceAll(toDel, "");
+                BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/notes/" + fileName));
+                String line;
+                StringBuilder content = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                }
+
+                Note note = new Note(fileName, content.toString());
+                allNotes.add(note);
+            }
+        } catch (Exception e) {
+            System.out.println();
+            e.printStackTrace();
+        }
+    }
+
 
     public void createNote(String title, String content) {
         try {
             Note note = new Note(title, content);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"/notes/" + note.getContent() +".txt"));
+            allNotes.add(note);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"/notes/" + note.getTitle() +".txt"));
             writer.write(note.getContent());
             writer.close();
         } catch (IOException ioException) {
@@ -26,51 +56,29 @@ public class NoteService {
 
 
     public void listNotesToCommandLine() {
-        try {
-            File fileFolder = new File(System.getProperty("user.dir") + "/notes");
-            File[] files = fileFolder.listFiles();
 
-            for (File file : files) {
-                System.out.println(file);
-            }
-        } catch (Exception e) {
-            System.out.println();
-            e.printStackTrace();
+        for (Note note : allNotes) {
+            System.out.println("Title: " + note.getTitle() + "  -> Content: " + note.getContent());
         }
     }
 
+
     public String listAllNotes() {
         StringBuilder res = new StringBuilder();
-        String response;
 
-        File fileFolder = new File(System.getProperty("user.dir") + "/notes");
-        File[] files = fileFolder.listFiles();
-        String toDel = System.getProperty("user.dir") + "/notes/";
-
-        for (File file : files) {
-            String fileName = file.toString().replaceAll(toDel, "");
-            res.append(fileName + "\n");
+        for (Note note : allNotes) {
+            res.append(note.getTitle() + "\n");
         }
-        response = res.toString();
-        return response;
+        return res.toString();
     }
 
 
     public void listSpecificNoteToCommandLine(String title) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/notes/" + title +".txt"));
-            String line;
 
-            while((line = reader.readLine()) != null) {
-                System.out.println(line);
+        for (Note note : allNotes) {
+            if (note.getTitle().equals(title)) {
+                System.out.println("Content: " + note.getContent());
             }
-            reader.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-            e.printStackTrace();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
         }
     }
 
@@ -78,12 +86,9 @@ public class NoteService {
     public String returnSpecificNote(Map<String,String> params) throws IOException {
         String response = "";
 
-        File fileFolder = new File(System.getProperty("user.dir") + "/notes");
-        File[] files = fileFolder.listFiles();
-
-        for (File file : files) {
-            if (file.toString().contains(params.get("title"))) {
-                response = Files.readString(Path.of(file.getPath()));
+        for (Note note : allNotes) {
+            if (note.getTitle().contains(params.get("title"))) {
+                response = note.getContent();
             }
         }
         return response;
